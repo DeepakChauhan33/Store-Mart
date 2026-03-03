09fa15dbde99c99748a6d5d729fe4ce3100493a5
deepakchauhan33 <deep651965@gmail.com>
2026-03-03 23:59:21 +0530

    Update

--- src/Pages/Order/OrderPage.jsx
+++ src/Pages/Order/OrderPage.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 
 import { LuBox } from "react-icons/lu";
+import { useNavigate } from 'react-router-dom';
 
 
 const OrderPage = () => {
+
+  const navigate = useNavigate();
   return (
     <section className=' p-2 py-4'>
 
       <div>
-        <h2 className='text-4xl font-light'>My Wishlist</h2>
+        <h2 className='text-4xl font-light'>My Orders</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, autem!</p>
       </div>
 
       <div className='h-96 flex justify-center items-center border'>
-        <div className='flex flex-col justify-center items-center'>
-          <LuBox />
-          <p>No items in your wishlist</p>
+        <div className='flex flex-col justify-center items-center gap-4'>
+          <LuBox className='text-7xl text-gray-400' />
+          <p>No items in your orders</p>
 
-          <button>
-            <a href="/products" className='bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 transition-colors duration-200'>Shop Now</a>
+          <button
+            onClick={() => navigate("/products")}
+            className='bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 transition-colors duration-200'>
+            Shop Now
           </button>
         </div>
       </div>
--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Wishlist = () => {
+
+  const navigate = useNavigate();
+
   return (
-    <div>
-      
-    </div>
+    <section className=' p-2 py-4'>
+
+      <div>
+        <h2 className='text-4xl font-light'>My Wishlist</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, autem!</p>
+      </div>
+
+      <div className='h-96 flex justify-center items-center border'>
+        <div className='flex flex-col justify-center items-center gap-4'>
+          <LuBox className='text-7xl text-gray-400' />
+          <p>No items in your wishlist</p>
+
+          <button
+            onClick={() => navigate("/products")}
+            className='bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 transition-colors duration-200'>
+            Shop Now
+          </button>
+        </div>
+      </div>
+    </section>
   )
 }
 
