b944a94ae698e4c436062a6b39b87805e8c9d883
deepakchauhan33 <deep651965@gmail.com>
2026-03-03 23:52:52 +0530

    Add elements in Order page

--- src/Pages/Order/OrderPage.jsx
+++ src/Pages/Order/OrderPage.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 
+import { LuBox } from "react-icons/lu";
+
+
 const OrderPage = () => {
   return (
-    <div className=' p-2 py-4'>
-      <h2 className='text-4xl font-light'>My Wishlist</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, autem!</p>
-    </div>
+    <section className=' p-2 py-4'>
+
+      <div>
+        <h2 className='text-4xl font-light'>My Wishlist</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, autem!</p>
+      </div>
+
+      <div className='h-96 flex justify-center items-center border'>
+        <div className='flex flex-col justify-center items-center'>
+          <LuBox />
+          <p>No items in your wishlist</p>
+
+          <button>
+            <a href="/products" className='bg-gray-800 text-white px-4 py-2 rounded-lg hover:bg-gray-700 transition-colors duration-200'>Shop Now</a>
+          </button>
+        </div>
+      </div>
+    </section>
   )
 }
 
